3dc44b525d0a9239cdf44870d1da2c04388d4a30
Bard <bard.bloom@10gen.com>
2013-11-11 12:28:55 -0500

    deeds

--- beasts.go
+++ beasts.go
@@ -5,11 +5,14 @@ import (
 	"fmt"
 	"github.com/robertkrimen/otto"
 	//	"github.com/robertkrimen/otto/underscore"
+	"math/rand"
 	"time"
 )
 
 var _ fmt.Stringer = nil
 var _ = util.Ignore()
+
+var Rando = rand.New(rand.NewSource(123))
 
 type Javascript string
 
@@ -22,30 +25,57 @@ type Beast interface {
 }
 
 type BeastMaster interface {
-	BeastJustDid(js Javascript)
+	BeastAboutToDo(js Javascript)
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+
+type Deed struct {
+	Name string
+	Js   Javascript
+	Fun  func(*Shrew)
+}
+
+func randomDeed(deeds []Deed) Deed {
+	k := Rando.Int31n(int32(len(deeds)))
+	return deeds[k]
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
 type Shrew struct {
-	ottoInterp  otto.Otto
-	beastMaster BeastMaster
+	Name         string
+	OttoInterp   otto.Otto
+	NDeedsToDo   int
+	BeastMaster1 BeastMaster
+	Deeds        []Deed
 }
 
 func (shrew *Shrew) Otto() otto.Otto {
-	return shrew.ottoInterp
+	return shrew.OttoInterp
 }
 
 func (shrew *Shrew) BeastMaster() BeastMaster {
-	return shrew.beastMaster
+	return shrew.BeastMaster1
 }
 
 func (shrew *Shrew) DoAt(js Javascript, t time.Time) {
 	fmt.Printf("ӜΘ7こ– I should schedule –  js=%v, t=%v\n", js, t)
 }
 
+func (this *Shrew) String() string {
+	return fmt.Sprintf("Shrew(%v)", this.Name)
+}
+
 func (shrew *Shrew) Run() {
-	fmt.Printf("ӜΝ1や– I run!\n")
+	for i := 0; i < shrew.NDeedsToDo; i++ {
+		deed := randomDeed(shrew.Deeds)
+		fmt.Printf("ӜΔ7に– %v doing %v\n", shrew, deed.Name)
+		shrew.BeastMaster().BeastAboutToDo(deed.Js)
+		deed.Fun(shrew)
+	}
 }
 
 func (shrew *Shrew) Stop() {
@@ -59,7 +89,7 @@ var _ Beast = &Shrew{}
 type Wombat struct {
 }
 
-func (wombat *Wombat) BeastJustDid(js Javascript) {
+func (wombat *Wombat) BeastAboutToDo(js Javascript) {
 	fmt.Printf("Ӝε0け– I, Wombat, am ignoring –  js=%v\n", js)
 }
 
