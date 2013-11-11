55820dceeca7ce70196324240826d9d8d275750f
Bard <bard.bloom@10gen.com>
2013-11-11 11:42:53 -0500

    sketch

--- beasts.go
+++ beasts.go
@@ -3,6 +3,8 @@ package main
 import (
 	"com.tengen/cm/util"
 	"fmt"
+	"github.com/robertkrimen/otto"
+	//	"github.com/robertkrimen/otto/underscore"
 	"time"
 )
 
@@ -12,13 +14,53 @@ var _ = util.Ignore()
 type Javascript string
 
 type Beast interface {
-	DoAt(js Javascript, t time.Time)
-	Perform(actionAsJs string, beastmaster BeastMaster, action func())
+	DoAt(js Javascript, t time.Time) // Tell the beast to do something
 	Run()
 	Stop()
+	Otto() otto.Otto
+	BeastMaster() BeastMaster
 }
 
 type BeastMaster interface {
+	BeastJustDid(js Javascript)
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+type Shrew struct {
+	ottoInterp  otto.Otto
+	beastMaster BeastMaster
+}
+
+func (shrew *Shrew) Otto() otto.Otto {
+	return shrew.ottoInterp
+}
+
+func (shrew *Shrew) BeastMaster() BeastMaster {
+	return shrew.beastMaster
+}
+
+func (shrew *Shrew) DoAt(js Javascript, t time.Time) {
+	fmt.Printf("ӜΘ7こ– I should schedule –  js=%v, t=%v\n", js, t)
+}
+
+func (shrew *Shrew) Run() {
+	fmt.Printf("ӜΝ1や– I run!\n")
+}
+
+func (shrew *Shrew) Stop() {
+	fmt.Printf("ӜΝ1や– I stop!!\n")
+}
+
+var _ Beast = &Shrew{}
+
+////////////////////////////////////////////////////////////////////////////////
+
+type Wombat struct {
+}
+
+func (wombat *Wombat) BeastJustDid(js Javascript) {
+	fmt.Printf("Ӝε0け– I, Wombat, am ignoring –  js=%v\n", js)
 }
 
 ////////////////////////////////////////////////////////////////////////////////
